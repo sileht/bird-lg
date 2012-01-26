7991945f5f5820b15a2592271707906c5697d500
Mehdi Abaakouk <sileht@sileht.net>
2012-01-26 23:24:15 +0100

    Add some link

--- lg.py
+++ lg.py
@@ -38,7 +38,7 @@ def add_links(text):
 			line = re.sub(r'AS(\d+)', r'<a href="/whois/\1" class="whois">AS\1</a>',line)
 			line = re.sub(r'(\d+\.\d+\.\d+\.\d+)', r'<a href="/whois/\1" class="whois">\1</a>',line)
 			hosts = "/".join(request.path.split("/")[2:])
-			line = re.sub(r'\[(\w+)\s+((|\d\d\d\d-\d\d-\d\d\s)(|\d\d:)\d\d:\d\d|\w\w\w\d\d)\]', r'[<a href="/detail/%s?q=\1">\1</a> \2]' % hosts, line)
+			line = re.sub(r'\[(\w+)\s+((|\d\d\d\d-\d\d-\d\d\s)(|\d\d:)\d\d:\d\d|\w\w\w\d\d)', r'[<a href="/detail/%s?q=\1">\1</a> \2' % hosts, line)
 			ret_text.append(line)
 	return "\n".join(ret_text)
 
