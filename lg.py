4cdb730b59a643b8eee4ea38465a292076e43fcb
Mehdi Abaakouk <sileht@sileht.net>
2012-01-27 19:12:59 +0100

    Add copyright header

--- lg.py
+++ lg.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: ts=4
+###
+#
+# Copyright (c) 2006 Mehdi Abaakouk
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+#
+###
 
 import sys
 import os
@@ -14,14 +34,6 @@ from flask import Flask, render_template, jsonify, redirect, session, request, a
 
 app = Flask(__name__)
 app.config.from_pyfile('lg.cfg')
-
-
-#def same_origin_policy_hander(resp):
-#	resp.headers["Access-Control-Allow-Origin"] =  "*"
-#	return resp
-#
-#app.after_request(same_origin_policy_hander)
-#
 
 def add_links(text):
 	if type(text) in [ str, unicode ]:
--- test.py
+++
@@ -1,6 +0,0 @@
-
-from bird import BirdSocketSingleton
-
-s = BirdSocketSingleton("h3", 9994)
-print s.cmd("show protocols")
-print s.cmd("show protocols all TETANEUTRAL")
