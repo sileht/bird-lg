c67f4644ae84a3ff569d2765c80ebb09a27a2f94
dsx <dsx@droidnest.org>
2015-01-21 12:03:55 -0500

    PEP8 compliance

--- toolbox.py
+++ toolbox.py
@@ -28,17 +28,20 @@ resolv = resolver.Resolver()
 resolv.timeout = 0.5
 resolv.lifetime = 1
 
+
 def resolve(n, q):
-	return str(resolv.query(n,q)[0])
+    return str(resolv.query(n, q)[0])
+
 
 def mask_is_valid(n):
-	if not n: 
-		return True
-	try:
-		mask = int(n)
-		return ( mask >= 1 and mask <= 128)
-	except:
-		return False
+    if not n:
+        return True
+    try:
+        mask = int(n)
+        return (mask >= 1 and mask <= 128)
+    except:
+        return False
+
 
 def ipv4_is_valid(n):
     try:
@@ -47,6 +50,7 @@ def ipv4_is_valid(n):
     except socket.error:
         return False
 
+
 def ipv6_is_valid(n):
     try:
         socket.inet_pton(socket.AF_INET6, n)
@@ -54,22 +58,25 @@ def ipv6_is_valid(n):
     except socket.error:
         return False
 
-def save_cache_pickle(filename, data):
-	output = open(filename, 'wb')
-	pickle.dump(data, output)
-	output.close()
 
-def load_cache_pickle(filename, default = None):
-	try:
-		pkl_file = open(filename, 'rb')
-	except IOError:
-		return default
-	try:
-		data = pickle.load(pkl_file)
-	except:
-		data = default
-	pkl_file.close()
-	return data
+def save_cache_pickle(filename, data):
+    output = open(filename, 'wb')
+    pickle.dump(data, output)
+    output.close()
+
+
+def load_cache_pickle(filename, default=None):
+    try:
+        pkl_file = open(filename, 'rb')
+    except IOError:
+        return default
+    try:
+        data = pickle.load(pkl_file)
+    except:
+        data = default
+    pkl_file.close()
+    return data
+
 
 def unescape(s):
     want_unicode = False
