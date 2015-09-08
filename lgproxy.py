9de7dee49ce49c669f41100046339a62962d8b1d
Martin Pels <martin@rodecker.nl>
2015-09-08 11:46:07 +0200

    Specify bird socket file location in config

--- lgproxy.py
+++ lgproxy.py
@@ -96,8 +96,8 @@ def traceroute():
 def bird():
     check_accesslist()
 
-    if request.path == "/bird": b = BirdSocket(file="/var/run/bird.ctl")
-    elif request.path == "/bird6": b = BirdSocket(file="/var/run/bird6.ctl")
+    if request.path == "/bird": b = BirdSocket(file=app.config.get("BIRD_SOCKET"))
+    elif request.path == "/bird6": b = BirdSocket(file=app.config.get("BIRD6_SOCKET"))
     else: return "No bird socket selected"
 
     query = request.args.get("q","")
